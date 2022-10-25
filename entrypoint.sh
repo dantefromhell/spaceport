469b20ff20edb5b964815e25c29df13e13966a6b
dantefromhell <90508808+dantefromhell@users.noreply.github.com>
2022-10-25 20:22:09 +0000

    [fix] nodeJS IP binding inside container
    The default `TW5-Bob` configuration binds the nodeJS process to `127.0.0.1` which inside a docker image makes for an unaccessible port.
    
    Using `sed` to "edit" the config file `settings.json` in place and make the process listen on all ports during container startup should fix things.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ if [ ! -d /var/local/tiddlywiki/wikis ]
 then
   mkdir /var/local/tiddlywiki/wikis
   cp -r /usr/local/tiddlywiki/plugins/OokTech/Bob/MultiUserWiki /var/local/tiddlywiki/wikis/BobWiki
+  sed -i 's/127.0.0.1/0.0.a.0/' /var/local/tiddlywiki/wikis/BobWiki/settings/settings.json
 fi
 
 # Start the tiddlywiki server
